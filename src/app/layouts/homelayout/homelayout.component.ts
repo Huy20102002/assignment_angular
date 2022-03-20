8fe309ac301cafac81a9e8003fc0cd91a1fef1e9
Huy20102002 <huy.dev.2010@gmail.com>
2022-03-20 11:11:38 +0700

    Finish Router

--- src/app/layouts/homelayout/homelayout.component.ts
+++ src/app/layouts/homelayout/homelayout.component.ts
@@ -3,12 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-homelayout',
   templateUrl: './homelayout.component.html',
-  styleUrls: ['./homelayout.component.css']
+  styleUrls: ['./homelayout.component.css'],
 })
 export class HomelayoutComponent implements OnInit {
-
+  
   constructor() { }
-
   ngOnInit(): void {
   }
 
--- src/app/screen/subjects/subjects.component.ts
+++ src/app/screen/subjects/subjects.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-subjects',
   templateUrl: './subjects.component.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  Subject: Array<any> = [];
   ngOnInit(): void {
+    this.http.get<any>("http://localhost:3001/subjects")
+    .subscribe(data=>{
+      this.Subject = data;
+    })
   }
 
 }
